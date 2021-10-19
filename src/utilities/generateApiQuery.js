f4f4c5a99e0011bb96d5f97821dc082109bf3e98
Jack Laird <jack.laird0@gmail.com>
2021-10-19 14:37:30 -0600

    Add Spacing

--- src/utilities/generateApiQuery.js
+++ src/utilities/generateApiQuery.js
@@ -1,5 +1,6 @@
 const generateApiQuery = (queryInfo) => {
   const { text, language = "", filter = "" } = queryInfo;
+
   return `https://api.github.com/search/repositories?q=${encodeURIComponent(
     text
   )}+language:${encodeURIComponent(language)}&sort=${encodeURIComponent(
